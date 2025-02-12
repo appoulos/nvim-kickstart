42dfa72447f96274ff89f1e723c8550a1b1b6bbf
Anthony Poulos <anthonypoulos@yahoo.com>
2025-02-12 09:46:14 -0600

    .

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -524,6 +524,18 @@ vim.keymap.set('x', '<M-k>', ":m '>-3<CR>gv+gv", { desc = 'Block move up' })
 -- [">"] = ">gv",
 vim.keymap.set('v', '<', '<gv', { desc = 'Unindent' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent' })
+-- jdtls
+vim.keymap.set('n', '<M-o>', ":lua require'jdtls'.organize_imports()<CR>", { desc = 'jdtls Organize imports' })
+-- nnoremap <A-o> <Cmd>lua require'jdtls'.organize_imports()<CR>
+-- nnoremap crv <Cmd>lua require('jdtls').extract_variable()<CR>
+-- vnoremap crv <Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>
+-- nnoremap crc <Cmd>lua require('jdtls').extract_constant()<CR>
+-- vnoremap crc <Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>
+-- vnoremap crm <Esc><Cmd>lua require('jdtls').extract_method(true)<CR>
+-- " If using nvim-dap
+-- " This requires java-debug and vscode-java-test bundles, see install steps in this README further below.
+-- nnoremap <leader>df <Cmd>lua require'jdtls'.test_class()<CR>
+-- nnoremap <leader>dn <Cmd>lua require'jdtls'.test_nearest_method()<CR>
 
 -- apoulos from: https://vi.stackexchange.com/questions/42740/how-to-load-the-global-vim-object-so-lua-language-server-stops-complaining
 -- error: caused Lua Language server refused to load this directory on nvim startup
